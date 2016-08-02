c1f9fe348ef0bb610b0a7b76ab0cd2cecdc22ed1
Richard Williams <richwilliamsuk@MacBook-Pro.local>
2016-08-02 19:16:15 +0100

    Added favourite words and create json and all other files

---
+++ dist/favourite-words.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const fs = require('fs');
+
+// filename from argument
+var fullFilename = process.argv.slice(2).toString(),
+    chatToJson;
+
+let fnArr = fullFilename.split("/"),
+    fn = fnArr[fnArr.length - 1];
+
+if (fn) {
+  fs.readFile(`./json/${ fn }`, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    const favWords = new FavouriteWords(data);
+  });
+} else {
+  console.log('No filename specified.');
+}
+
+class FavouriteWords {
+  constructor(data) {
+    let json = JSON.parse(data),
+        entries = json.entries;
+
+    // split words to users
+    this.words = {};
+    this.userWords = {};
+
+    for (var i = 0, len = entries.length; i < len; i++) {
+      let entry = entries[i],
+          entryWords = this.splitSentence(entry);
+    }
+
+    this.showFavouriteWords(this.words);
+
+    // write to json file
+    let jsonString = JSON.stringify(this.userWords),
+        jsonTarget = `./json/${ fn.slice(0, -5) }-fav-words.json`;
+
+    if (jsonString && jsonTarget) {
+      fs.writeFile(jsonTarget, jsonString, err => {
+        if (err) {
+          throw err;
+        }
+
+        console.log(`Saved to ${ jsonTarget }.`);
+      });
+    }
+  }
+
+  showFavouriteWords(wordsObj) {
+
+    for (var i in wordsObj) {
+      // create user obj
+      let userWordsObj = {},
+          userWords = wordsObj[i].sort();
+
+      for (var w = 0, len = userWords.length; w < len; w++) {
+        if (!userWordsObj[userWords[w]]) {
+          userWordsObj[userWords[w]] = 0;
+        }
+
+        userWordsObj[userWords[w]] += 1;
+      }
+
+      this.userWords[i] = userWordsObj;
+    }
+  }
+
+  splitSentence(entry) {
+    const charRe = /[\(\)\{\}\[\],.\/:0-9\-!?]/g;
+
+    let sentence = entry.content.replace(charRe, '');
+
+    if (!this.words[entry.user]) {
+      this.words[entry.user] = [];
+    }
+
+    // concatenate new words and filter out common words
+    this.words[entry.user] = this.words[entry.user].concat(sentence.split(/\s+/).filter(this.filterWords));
+  }
+
+  filterWords(word) {
+    // filter words, check word length
+    let wordRe = /^(and|or|the|why|how|but|this|that|is|are|our|from|of|with|yeh|yeah|not|can|too|what|got)$/i;
+
+    if (word.length < 2 || word.length > 20 || wordRe.test(word)) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+module.exports = FavouriteWords;
---
+++ src/create-json.js
@@ -0,0 +1,97 @@
+'use strict';
+
+const fs = require('fs');
+
+// filename from argument
+var fullFilename = process.argv.slice(2).toString(),
+  chatToJson;
+
+let fnArr = fullFilename.split("/"),
+  fn = fnArr[fnArr.length - 1];
+
+if ( fn ) {
+  fs.readFile(`./txt/${fn}`, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    let json = new ChatToJson(data),
+      jsonString = JSON.stringify(json),
+      jsonTarget = `./json/${fn.slice(0,-4)}.json`;
+
+    if ( jsonString && jsonTarget ) {
+      fs.writeFile(jsonTarget, jsonString, (err) => {
+        if ( err ) {
+          throw err;
+        }
+
+        console.log(`Saved to ${jsonTarget}.`);
+      });
+    }
+  });
+} else {
+  console.log('No filename specified.');
+}
+
+class ChatToJson {
+  constructor(data) {
+
+    let textArr = data.split('\n'),
+      entriesArr = [];
+
+    for ( let i = 0, len = textArr.length; i < len; i ++ ) {
+
+      if ( textArr[i].length > 22 ) {
+        let entry = {},
+          entryStr = '',
+          textStrDateChunk = textArr[i].slice(0,20),
+          textStrTextChunk = textArr[i].slice(22);
+
+        // check for colon ( verify is user comment )
+        let textStrTextChunkIndex = textStrTextChunk.indexOf(':');
+        if ( textStrTextChunkIndex >= 0 ) {
+          let content = this.validateContent(textStrTextChunk.slice(textStrTextChunkIndex + 2));
+
+          if ( content ) {
+            entry.content = content;
+            entry.date = this.formatDate(textStrDateChunk);
+            entry.user = textStrTextChunk.slice(0, textStrTextChunkIndex);
+
+            entriesArr.push(entry);
+          }
+        }
+      }
+    }
+
+    return {
+      entries : entriesArr
+    }
+  }
+
+  validateContent(str) {
+    str = str.replace(/[\r\n]+/g,'');
+    if ( str.length && str !== "<image omitted>" && str !== "<audio omitted>" && str !== "<video omitted>" ) {
+      return str;
+    }
+
+    return null;
+  }
+
+  formatDate(str) {
+
+    let strToInt = (r1, r2) => {
+      return parseInt(str.slice(r1, r2));
+    }
+
+    let D = strToInt(0,2),
+      M = strToInt(3,5) - 1,
+      Y = strToInt(6,10),
+      h = strToInt(12,14),
+      m = strToInt(15,17),
+      s = strToInt(18,20);
+
+    return new Date(Y,M,D,h,m,s);
+  }
+}
+
+module.exports = ChatToJson;
